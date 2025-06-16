7d2a094bbb8910a8275feb24853fadf00da052ed
mysptr1906 <m.saputra1906@gmail.com>
2025-06-16 19:12:32 +0700

    add new steps

--- steps/global_steps.go
+++ steps/global_steps.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
 	"log"
+	"strconv"
 	"time"
 	"web-auto-go-framework/config"
 	"web-auto-go-framework/engine"
@@ -21,6 +22,23 @@ func visitUrl(url string) error {
 
 func click(locator string) error {
 	return e.Click(locator)
+}
+
+func rightClick(locator string) error {
+	return e.RightClick(locator)
+}
+
+func clickMultipleTimes(locator string, timesStr string) error {
+	times, err := strconv.Atoi(timesStr)
+	if err != nil {
+		return fmt.Errorf("click time not valid: '%s'. value must be number", timesStr)
+	}
+
+	if times <= 0 {
+		return fmt.Errorf("click must me more than 0")
+	}
+
+	return e.ClickMultipleTimes(locator, times)
 }
 
 func fill(locator string, text string) error {
@@ -42,9 +60,49 @@ func assertTextPresent(expectedText string) error {
 		return fmt.Errorf("text not found: %s", expectedText)
 	}
 
-	fmt.Printf("Text found: %s\n", expectedText)
-	_ = el // we don’t need to use it, just confirm it exists
+	_ = el
 	return nil
+}
+
+func seeTextEqualInElement(locator, expectedText string) error {
+	return e.IsTextEqualInElement(locator, expectedText)
+}
+
+func isTextContains(expectedText string) error {
+	return e.IsTextContains(expectedText)
+}
+
+func isTextContainsInElement(locator, expectedText string) error {
+	return e.IsTextContainsInElement(locator, expectedText)
+}
+
+func isTextMatchingRegexInElement(locator, pattern string) error {
+	return e.AssertTextMatchingRegexInElement(locator, pattern)
+}
+
+func isTextMatchingRegex(pattern string) error {
+	return e.AssertTextMatchingRegex(pattern)
+}
+
+func hover(locator string) error {
+	return e.Hover(locator)
+}
+
+func scrollToElement(locator string) error {
+	return e.ScrollToElement(locator)
+}
+
+func scrollInDirection(direction, timeStr string) error {
+	times := 1
+	var err error
+
+	if timeStr != "" {
+		times, err = strconv.Atoi(timeStr)
+		if err != nil {
+			return fmt.Errorf("invalid time value '%s', must be a number", timeStr)
+		}
+	}
+	return e.ScrollInDirection(direction, times)
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
@@ -74,6 +132,16 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 
 	ctx.Step(`^(?:I |i |User |user |Client |client )?visit "([^"]*)"$`, visitUrl)
 	ctx.Step(`^(?:I |i |User |user |Client |client )?click "([^"]*)"$`, click)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?right click "([^"]*)"$`, rightClick)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?click "([^"]*)" "([^"]*)" times$`, clickMultipleTimes)
 	ctx.Step(`^(?:I |i |User |user |Client |client )?fill "([^"]*)" with "([^"]*)"$`, fill)
 	ctx.Step(`^(?:I |i |User |user |Client |client )?assert text "([^"]*)"$`, assertTextPresent)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?see text on "([^"]*)" equal to "([^"]*)"$`, seeTextEqualInElement)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?see text contains "([^"]*)"$`, isTextContains)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?see text contains "([^"]*)" inside "([^"]*)"$`, isTextContainsInElement)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?see text matching regex "([^"]*)"$`, isTextMatchingRegex)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?see text matching regex "([^"]*)" inside "([^"]*)"$`, isTextMatchingRegexInElement)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?hover "([^"]*)"$`, hover)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?scroll to element "([^"]*)"$`, scrollToElement)
+	ctx.Step(`^(?:I |i |User |user |Client |client )?scroll (up|down|left|right)(?: "([^"]*)" times)?$`, scrollInDirection)
 }
